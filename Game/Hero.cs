68939c91e3bcfda9566c0794478ee0c4efff2b66
98Miquelle11 <michalkuropieska@gmail.com>
2025-06-08 00:02:07 +0200

    the core

---
+++ Game/Hero.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    enum Colors // nadanie poszczególnym bohaterom kolorów
+    {
+        red, green
+    }
+    abstract class Hero // nie chcemy instancjonować tej klasy, dlatego robimy ją abstrakcyjną
+    {
+        public string Name { get; } // nazwa tylko do odczytu, ponieważ nie chcę jej zmieniać w trakcie programu
+        public int FullHP { get; } // ta właściwość również nie będzie zmieniana w trakcie trwania programu
+        private int actualHP;
+
+        // Name i FullHP będziemy ustawiać z konstruktora wewnątrz
+
+        public int ActualHP
+        {
+            get { return actualHP; }
+            set
+            {   // będziemy ją zmieniać pod pewnymi warunkami
+                if (value < 0)
+                {
+                    actualHP = 0;
+                }
+                else if (value > FullHP)
+                {
+                    actualHP = FullHP;
+                }
+                else actualHP = value;
+
+            }
+        }
+        public Colors Color { get; set; } // będziemy je ustawiać z zewnątrz
+        
+        protected Random rnd = new Random();
+
+        public Hero(string name, int fullhp, Colors color)
+        {
+            Name = name;
+            FullHP = fullhp;
+            ActualHP = FullHP;
+            Color = color;
+        }
+
+        public abstract void DefaultAttack(Hero hero); // obiekt Hero będzie obiektem atakowanym
+        public abstract void Heal();
+        public override string ToString()
+        {
+            return $"{Name} - {ActualHP}/{FullHP}";
+        }
+    }
+}
---
+++ Game/Wizard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Wizard : Hero
+    {
+        public Wizard(string name, int fullhp, Colors color) : base(name, fullhp, color)
+        {
+        }
+
+        public override void DefaultAttack(Hero hero)
+        {
+            int hp = rnd.Next(100,151); // losowa wartość między 100 a 150
+            hero.ActualHP -= hp;
+            Console.WriteLine($"\nGracz {Name} zadał {hp} punktów obrażeń graczowi {hero.Name}.");
+        }
+
+        public override void Heal()
+        {
+            int hp = rnd.Next(100, 201);
+            ActualHP += hp;
+            Console.WriteLine($"\nGracz {Name} uzdrowił się za {hp} punktów życia.");
+        }
+    }
+}
